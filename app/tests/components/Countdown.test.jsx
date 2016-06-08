6c9629c4d91996dc33a1bc3252baa2157701c7ad
philippschulte <philipp.schulte@ymail.com>
2016-06-08 16:17:43 -0600

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -7,23 +7,22 @@ var TestUtils = require('react-addons-test-utils');
 var Countdown = require('Countdown');
 
 describe('Countdown', () => {
+  var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
   it('should exist', () => {
     expect(Countdown).toExist();
   });
 
   describe('handleSetCountdown', () => {
-    var countdown = TestUtils.renderIntoDocument(<Countdown/>);
-    var seconds = 10;
-
     it('should set state to started and countdown', () => {
-      countdown.handleSetCountdown(seconds);
+      countdown.handleSetCountdown(10);
 
-      expect(countdown.state.count).toBe(seconds);
+      expect(countdown.state.count).toBe(10);
       expect(countdown.state.countdownStatus).toBe('started');
     });
 
     it('should decrease count variable', (done) => {
-      countdown.handleSetCountdown(seconds);
+      countdown.handleSetCountdown(10);
 
       setTimeout(() => {
         expect(countdown.state.count).toBe(9);
@@ -38,6 +37,30 @@ describe('Countdown', () => {
         expect(countdown.state.count).toBe(0);
         done();
       }, 3001);
+    });
+  });
+
+  describe('handleStatusChange', () => {
+    it('should pause countdown on paused status', (done) => {
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should reset count on stopped', (done) => {
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
